25c7b10334622900f3c5549da8014631c3e21979
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-08 22:46:03 +0300

    Update ParentsController.php

--- app/Http/Controllers/ParentsController.php
+++ app/Http/Controllers/ParentsController.php
@@ -14,4 +14,16 @@ class ParentsController extends Controller
         $Parent->Nickname=request('Nickname');
         $Parent->save();
     }
+    public function Login($Email,$Pass){
+        $User=Parents::all()->where("Email",$Email)->where("Password",$Pass);
+        foreach($User as $Fetched){
+        $Results[0]=$Fetched->Email;
+        $Results[1]=$Fetched->Password;
+        $Results[2]=$Fetched->Nickname;
+        if($Results == null){
+            return "Empty";
+        }else{
+        return json_encode(['User'=>$Results]);   
+        }
+        }
 }
