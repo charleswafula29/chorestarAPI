33ede120c40fbdfe682ac8ad7f37c86f6c58b8c2
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-08 21:16:06 +0300

    Add files via upload

---
+++ database/migrations/2018_07_08_174802_create_children_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Email');
+            $table->string('ChildName');
+            $table->string('ChildImage');
+            $table->string('Age');
+            $table->string('Points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
