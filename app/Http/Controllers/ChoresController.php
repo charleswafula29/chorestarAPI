0b7d23b4a4693381c6408e35c2935241f4216b46
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-10 02:42:10 +0300

    Update ChoresController.php

--- app/Http/Controllers/ChoresController.php
+++ app/Http/Controllers/ChoresController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Chores;
 
 class ChoresController extends Controller
 {
-    //
+    public function NewChore(){
+        $child=new Chores();
+        $child->ChoreName=request('ChoreName');
+        $child->ChoreImage=request('ChoreImage');
+        $child->ChorePoints=request('ChorePoints');
+        $child->Email=request('Email');
+        $child->ChildName=request('ChildName');
+        $child->save();
+    }
+    
+    public function FetchChores($Email){
+        $Chores=Chores::all()->where("Email",$Email);
+        return json_encode(['Chores'=>$Chores]);
+    }
 }
