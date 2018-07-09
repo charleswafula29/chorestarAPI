c6c93ad5d0ba10fbce8980a8a4fde9a4eb6b426c
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-09 22:09:43 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 Route::post('/Register','ParentsController@RegisterParent');
 Route::get('/Login/{Email}/{Pass}','ParentsController@Login');
+Route::get('/FetchProfile/{Email}','ChildrenController@FetchProfile');
 Route::post('/RegisterChild','ChildrenController@Register');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
