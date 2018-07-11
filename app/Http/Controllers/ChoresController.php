221ea1c9bf9ca1cd3b1cac95b60bf7a85631623d
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-11 04:13:25 +0300

    Update ChoresController.php

--- app/Http/Controllers/ChoresController.php
+++ app/Http/Controllers/ChoresController.php
@@ -18,7 +18,7 @@ class ChoresController extends Controller
     }
     
     public function FetchChores($Email){
-        $Chores=Chores::all()->where("Email",$Email)->where("Status","Pending");
+        $Chores=Chores::all()->where("Email",$Email)->where("Status","Pending")->first();
         return json_encode(['Chores'=>$Chores]);
     }
     
